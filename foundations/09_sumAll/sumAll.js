7989de8e42497ebb4c04d6d474917342e235d5bc
Andrei <danaila95andrei@gmail.com>
2026-03-26 23:47:35 +0200

    solved 9th js ex

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = (a, b) => {
+    if(!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0)
+        return "ERROR";
+    
+    if (a > b)
+        [a, b] = [b, a];
 
+    return (b * (b + 1) - (a - 1) * a) / 2;
 };
 
 // Do not edit below this line

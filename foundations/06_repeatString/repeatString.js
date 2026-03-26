42e234b7a8312518c64f5aacc341e45ad1082808
Andrei <danaila95andrei@gmail.com>
2026-03-26 21:47:48 +0200

    solved 6th js ex

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = (string, num) => {
+    if (num < 0)
+        return 'ERROR';
 
+    let res = "";
+    
+    while(num){
+        num--;
+        res += string;
+    }
+    
+    return res;
 };
 
 // Do not edit below this line

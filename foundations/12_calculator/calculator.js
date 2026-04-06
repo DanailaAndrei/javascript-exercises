db523684a8be73b606b1125b8dbb5a2aca419466
Andrei <danaila95andrei@gmail.com>
2026-04-06 23:25:28 +0300

    solved 12th ex

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((acc, elem) => acc + elem, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, elem) => acc * elem);
 };
 
-const power = function() {
-	
+const power = function(b, e) {
+	return Math.pow(b, e);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let result = 1;
+  while(n > 1){
+    result *= n;
+    n--;
+  }
+  return result;
 };
 
 // Do not edit below this line

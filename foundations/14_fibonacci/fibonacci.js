bcbe1b548d6848d718a9b3960c76f75b3c80af68
Andrei <danaila95andrei@gmail.com>
2026-04-07 00:37:30 +0300

    solved 14th ex

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = Number(n);
+    if(n == 0)
+        return 0;
+    if(n < 0)
+        return "OOPS";
+    if(n <= 2)
+        return 1;
+    let a = 1, b = 1;
+    while(n >= 3){
+        let aux = b;
+        b = a + b;
+        a = aux;
+        n--;
+    }
+    return b;
 };
 
 // Do not edit below this line

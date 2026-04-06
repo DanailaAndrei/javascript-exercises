24e315155d77d15e5c67a3c8a2466acfc0b8c833
Andrei <danaila95andrei@gmail.com>
2026-04-07 02:41:37 +0300

    solved 16th ex

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let maxAge = 0, currentYear = new Date().getFullYear(), index = 0;
 
+    arr.forEach((elem, ind) => {
+        let age;
+        if(elem.hasOwnProperty("yearOfDeath"))
+            age = elem.yearOfDeath - elem.yearOfBirth;
+        else age = currentYear - elem.yearOfBirth;
+        if(maxAge < age){
+            maxAge = age;
+            index = ind;
+        }
+    });
+
+    return arr[index];
 };
 
 // Do not edit below this line

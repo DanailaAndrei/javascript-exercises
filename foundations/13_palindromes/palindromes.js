e06f9efb42b5c44d9644c50306c7ed2d6d529964
Andrei <danaila95andrei@gmail.com>
2026-04-07 00:07:32 +0300

    solved 13th ex

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = [...str].filter(el => ".,:; !?".indexOf(el) == -1).join("").toLowerCase();
+    return str == [...str].reverse().join("");
 };
 
 // Do not edit below this line

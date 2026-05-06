27d195a62ecca03bb74a6f5f44229b22e46ef5a9
Andrei <danaila95andrei@gmail.com>
2026-05-07 00:27:05 +0300

    solved 2nd recursion ex

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,15 @@
-const contains = function() {
-  
+const contains = (o, v) => {
+    let ok = false;
+    for (const p in o) {
+        if(o[p] instanceof Object)
+            ok = contains(o[p], v);
+        if(ok == true)
+            return true;
+        if(o[p] === v || (Number.isNaN(v) && Number.isNaN(o[p])))
+            return true;
+    }
+    return ok;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;

3a251f33b507093f43569cebcbd854fd08720f5b
Andrei <danaila95andrei@gmail.com>
2026-05-08 19:48:35 +0300

    solved 3rd recursion problem

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,5 +1,14 @@
-const totalIntegers = function() {
-  
+const totalIntegers = (o) => {
+    if(typeof o != "object")
+        return;
+    let count = 0;
+    for (const [key, value] of Object.entries(o)) {
+        if(Number.isInteger(value))
+            count++;    
+        if(typeof value == "object" && value != null)
+            count += totalIntegers(value);
+    }
+    return count;
 };
   
 // Do not edit below this line

723c4a77cd23da00412757c96006a50e9265830a
Andrei <danaila95andrei@gmail.com>
2026-05-10 04:48:58 +0300

    solved 5th and last recursion exercise

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,5 +1,14 @@
-const pascal = function() {
-  
+const pascal = (n) => {
+    if(n < 2)
+        return [1];
+   
+    let res = [], before = [0, ...pascal(n-1), 0];
+
+    for(let i = 0; i < n; i++){
+        res.push(before[i] + before[i+1]);            
+    }
+
+    return res;
 };
   
 // Do not edit below this line
